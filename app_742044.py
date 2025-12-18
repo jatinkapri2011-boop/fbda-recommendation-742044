de96666a67365f76d738d482edf46c63aac91266
jatinkapri2011-boop <jatinkapri2011@gmail.com>
2025-12-18 10:48:09 +0530

    Update app_742044.py

--- app_742044.py
+++ app_742044.py
@@ -1,3 +1,10 @@
+import gdown
+import os
+
+def download_from_drive(file_id, output):
+    if not os.path.exists(output):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        gdown.download(url, output, quiet=False)
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -16,3 +23,11 @@ meta = pd.read_parquet("meta_742044.parquet")
 
 st.subheader("Sample Movies")
 st.dataframe(meta.head(10))
+# 🔽 DOWNLOAD LARGE FILES FROM GOOGLE DRIVE
+download_from_drive("1yXZQPeLB9dao-9bsnM1SRUNnRc2sWfL7", "svd_model_742044.pkl")
+download_from_drive("1h1brxLMYv8u4yH-_U70Kxwprv_u0hjEy", "tfidf_vectorizer_742044.pkl")
+download_from_drive("1YZfmBFBCc3AxKsoPm5E0CQ4tBT9GIRMY", "tfidf_matrix_742044.npz")
+download_from_drive("1a506s4IJqeunzTSQHg5LVSS0xnLl-Fpq", "embeddings_742044.npy")
+download_from_drive("17orGU6B1SMocR2y_Z_b_PKFK7O_TSyfu", "faiss_index_742044.index")
+download_from_drive("1kfK0x7hPQC9TvZwLprlQHFwJ4e8CcBqy", "meta_742044.parquet")
+download_from_drive("1PBUEUc8N1XvaPnX8x9dbXy4iT_tNBSAl", "sampled_10001_742044.csv")
